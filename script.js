4a053e6308a2c58def2e17d78921ea0af1cbcefd
maxmaps <maxschmitt94@gmail.com>
2017-09-05 21:18:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// create an array or object with a list of your favorite things to do in your free time
+// then, create a loop that prints each item in the console
+
+var favoritethings = ["guitar", "movies", "eating"];
+
+for (var i = 0; i < favoritethings.length; i++) {
+    console.log(favoritethings[i]);
+}
